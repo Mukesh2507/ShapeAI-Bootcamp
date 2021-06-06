fea84154972576db20e0a8ad32dc08af14fe0b98
Mukeshkumar Labana <74096849+Mukesh2507@users.noreply.github.com>
2021-06-06 12:08:14 +0530

    project files

---
+++ Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Note(){
+
+   return (
+         
+    <div className ="note">
+      <h1>JavaScript and React.js</h1>
+      <p>this is amazing bootcamp taken up by shaurya sinha
+        everything from scratch including js and react completely well
+        explained</p>
+
+
+
+      </div>
+);
+
+
+}
+export default Note;
